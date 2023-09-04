ccbce2204f85d4b4cb7b6248a099d6456e6791be
CN-Curriculum <138328232+CN-Curriculum@users.noreply.github.com>
2023-09-04 17:48:44 -0400

    Added conditonals coding exercise

---
+++ conditionals.js
@@ -0,0 +1,19 @@
+// Scenario:
+// You're building an application for an ice cream shop. They want a feature that 
+// recommends an ice cream flavor based on the current temperature in Fahrenheit.
+
+// 1. Create a variable named temperature any value between 32 and 68.
+
+
+// 2. Create a variable named recommendedFlavor. Do not assign a value.
+
+
+// 3. Implement the recommendation logic
+//     If the temperature is less than 50, recommend "Hot Chocolate Fudge".
+//     If the temperature is between 50 and 68 (inclusive), recommend "Caramel Crunch".
+//     If the temperature is greater than 68, recommend "Lemon Sorbet".
+
+
+// 4. Console log the recommended flavor.
+
+
