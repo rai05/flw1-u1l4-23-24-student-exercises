a6fe1093ae54d01237a95dcdec86c711b5179a97
CN-Curriculum <138328232+CN-Curriculum@users.noreply.github.com>
2023-09-06 09:33:27 -0400

    Add variables.js exercise

---
+++ variables.js
@@ -0,0 +1,15 @@
+// 1. Create a variable called `color` and assign it a string value of a color you like.
+
+
+// 2. Create a variable called `animal` and assign it a string value of an animal you like.
+
+
+// 3. Create a variable called `creature`. 
+// - The calue should be the concatenation of the variable `color` and `animal`.
+// - Include a space!
+
+
+
+
+// 4. Uncomment the `message` variable and log it to the console.
+// let message = `I was on a walk today and saw a ${creature}. It was odd.`
